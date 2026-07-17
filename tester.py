313dc5f32b1f066d5727f50dd38670732dfb911c
Ryan Kamau <ryankamau27@gmail.com>
2026-07-17 21:07:20 +0300

    resolved redownload on every extractor now uing cahe nd temp files

--- tester.py
+++ tester.py
@@ -1,7 +1,9 @@
-from inside import KNBSExtractor
+from knbs_scraper import KNBSExtractor
 
 sample_pdf = "https://www.knbs.or.ke/wp-content/uploads/2026/05/Leading-Economic-Indicators-March-2026.pdf"
 
 extractor = KNBSExtractor()
-results = extractor.get_inflation(sample_pdf)
+results = extractor.get_exchange_rates(sample_pdf)
 print(results)
+result =  extractor.get_exchange_rates(sample_pdf)
+print(result)
